70b5175c8fc27a7ed88e5a868f3fb68a40febf2c
Wesley Paulo <wesley_paulo00@hotmail.com>
2021-04-17 14:12:46 -0300

    Updates

---
+++ precache-manifest.31d14c930ec03e129c4ac7c295aab640.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf6991baf4e5a95df781feb4066f9f93",
+    "url": "/KitchenMemories/index.html"
+  },
+  {
+    "revision": "6beb8f730de72e3c3a02",
+    "url": "/KitchenMemories/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "546a756ab2a769d3ce31",
+    "url": "/KitchenMemories/static/css/main.bb027abf.chunk.css"
+  },
+  {
+    "revision": "6beb8f730de72e3c3a02",
+    "url": "/KitchenMemories/static/js/2.93413e1c.chunk.js"
+  },
+  {
+    "revision": "9353b4df7938eafd5f8e3a0cf5a68c12",
+    "url": "/KitchenMemories/static/js/2.93413e1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "546a756ab2a769d3ce31",
+    "url": "/KitchenMemories/static/js/main.af0780bd.chunk.js"
+  },
+  {
+    "revision": "e5bfce2cd58ea4549f0b",
+    "url": "/KitchenMemories/static/js/runtime-main.768c6188.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/KitchenMemories/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/KitchenMemories/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/KitchenMemories/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "96981abd070b30febe883201fe6aee05",
+    "url": "/KitchenMemories/static/media/logo.96981abd.png"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/KitchenMemories/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/KitchenMemories/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/KitchenMemories/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/KitchenMemories/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/KitchenMemories/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
